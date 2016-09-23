23022cf280e9c3108dd72930d401920a2a93f529
yeching <yq08051035@163.com>
2016-09-24 04:54:06 +0800

    new add

---
+++ js/index.js
@@ -0,0 +1,28 @@
+function display1(){
+    var oul=document.getElementById('catagory1'); 
+	if (oul.style.display=="none"){
+		oul.style.display="block";
+}
+else{oul.style.display="none";}
+}
+function display2(){
+    var oul=document.getElementById('catagory2'); 
+	if (oul.style.display=="none"){
+		oul.style.display="block";
+}
+else{oul.style.display="none";}
+}
+function display3(){
+    var oul=document.getElementById('catagory3'); 
+	if (oul.style.display=="none"){
+		oul.style.display="block";
+}
+else{oul.style.display="none";}
+}
+function display4(){
+    var oul=document.getElementById('catagory4'); 
+	if (oul.style.display=="none"){
+		oul.style.display="block";
+}
+else{oul.style.display="none";}
+}
